3adeac88e317ee5212aaa374098fbe58b39575b2
Mahiwhite <vdprajapti23@gmail.com>
2024-04-21 23:43:07 +0100

    My first push to repo

---
+++ JavaHomework/src/inheritanceque4/Cheetah.java
@@ -0,0 +1,14 @@
+package inheritanceque4;
+/* Write a Java program to create a class called Animal with a method named move().
+Create a subclass called Cheetah that overrides the move() method to run. */
+
+public class Cheetah extends Animal {
+    public void move(){
+        System.out.println("run");
+    }
+
+    public static void main(String[] args) {
+        Cheetah ch = new Cheetah();
+        ch.move();
+    }
+}
