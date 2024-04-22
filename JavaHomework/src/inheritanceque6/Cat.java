3915579289fd9c459ea3a519bd6cfb4db843cbdf
Mahiwhite <vdprajapti23@gmail.com>
2024-04-22 21:02:57 +0100

    My first push to repo

---
+++ JavaHomework/src/inheritanceque6/Cat.java
@@ -0,0 +1,14 @@
+package inheritanceque6;
+/* Write a Java program to create a base class Animal (Animal Family) with a method
+called Sound(). Create two subclasses Bird and Cat. Override the Sound() method in
+each subclass to make a specific sound for each animal. */
+
+public class Cat extends AnimalFamily {
+    public void Sound(){
+        System.out.println("Meow Meow Meow");
+    }
+    public static void main(String[] args) {
+        Cat c = new Cat();
+        c.Sound();
+    }
+}
